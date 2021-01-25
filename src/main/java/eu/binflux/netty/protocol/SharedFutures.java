4a3d5f4ab86db84ce7dbd010b45dfc2392c3e81c
jko <kobohd@gmail.com>
2021-01-25 16:35:56 +0100

    Optimized generics in SharedFutures

--- src/main/java/eu/binflux/netty/protocol/SharedFutures.java
+++ src/main/java/eu/binflux/netty/protocol/SharedFutures.java
@@ -16,55 +16,66 @@ public class SharedFutures {
         return futureMap.containsKey(futureId);
     }
 
-    public static <T> void addFuture(String futureId, CompletableFuture<T> completableFuture) {
+    public static <O> void addFuture(String futureId, CompletableFuture<O> completableFuture) {
         if (!hasFuture(futureId))
             futureMap.put(futureId, completableFuture);
+        completableFuture.whenComplete((t, e) -> removeFuture(futureId));
     }
 
     @SuppressWarnings("unchecked")
-    public static <T> CompletableFuture<T> getFuture(String futureId) {
-        return (CompletableFuture<T>) futureMap.getOrDefault(futureId, null);
+    public static <O> CompletableFuture<O> getFuture(String futureId) {
+        return (CompletableFuture<O>) futureMap.getOrDefault(futureId, null);
     }
 
     public static void removeFuture(String futureId) {
         futureMap.remove(futureId);
     }
 
-    public static <T> Map.Entry<String, CompletableFuture<T>> generateFuture() {
+    public static <O> Map.Entry<String, CompletableFuture<O>> generateFuture() {
         String futureId = createFutureId();
-        CompletableFuture<T> future = new CompletableFuture<>();
+        CompletableFuture<O> future = new CompletableFuture<>();
         addFuture(futureId, future);
-        future.whenComplete((t, e) -> removeFuture(futureId));
         return new AbstractMap.SimpleEntry<>(futureId, future);
     }
 
+    public static <O> Map.Entry<String, CompletableFuture<O>> generateFuture(Class<O> classO) {
+        return generateFuture();
+    }
 
-    public static <T> Map.Entry<String, CompletableFuture<HashMap<String, T>>> generateMapFuture() {
+    public static <O> Map.Entry<String, CompletableFuture<HashMap<String, O>>> generateMapFuture() {
         String futureId = createFutureId();
-        CompletableFuture<HashMap<String, T>> future = new CompletableFuture<>();
+        CompletableFuture<HashMap<String, O>> future = new CompletableFuture<>();
         addFuture(futureId, future);
-        future.whenComplete((t, e) -> removeFuture(futureId));
         return new AbstractMap.SimpleEntry<>(futureId, future);
     }
 
-    public static <T> Map.Entry<String, CompletableFuture<HashSet<T>>> generateSetFuture() {
+    public static <O> Map.Entry<String, CompletableFuture<HashMap<String, O>>> generateMapFuture(Class<O> classO) {
+        return generateMapFuture();
+    }
+
+    public static <O> Map.Entry<String, CompletableFuture<HashSet<O>>> generateSetFuture() {
         String futureId = createFutureId();
-        CompletableFuture<HashSet<T>> future = new CompletableFuture<>();
+        CompletableFuture<HashSet<O>> future = new CompletableFuture<>();
         addFuture(futureId, future);
-        future.whenComplete((t, e) -> removeFuture(futureId));
         return new AbstractMap.SimpleEntry<>(futureId, future);
     }
 
+    public static <O> Map.Entry<String, CompletableFuture<HashSet<O>>> generateSetFuture(Class<O> classO) {
+        return generateSetFuture();
+    }
 
-    public static <T> Map.Entry<String, CompletableFuture<List<T>>> generateListFuture() {
+    public static <O> Map.Entry<String, CompletableFuture<List<O>>> generateListFuture() {
         String futureId = createFutureId();
-        CompletableFuture<List<T>> future = new CompletableFuture<>();
+        CompletableFuture<List<O>> future = new CompletableFuture<>();
         addFuture(futureId, future);
-        future.whenComplete((t, e) -> removeFuture(futureId));
         return new AbstractMap.SimpleEntry<>(futureId, future);
     }
 
-    private static String createFutureId() {
+    public static <O> Map.Entry<String, CompletableFuture<List<O>>> generateListFuture(Class<O> classO) {
+        return generateListFuture();
+    }
+
+    public static String createFutureId() {
         int length = 32;
         String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvxyz";
         StringBuilder stringBuilder = new StringBuilder(length);
